a2faed11776edbcd1cc63049a0604a0a03c8c458
SofiaDrejmark <sofiadrejmark@gmail.com>
2022-08-07 21:19:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,179 @@
-import logo from './logo.svg';
-import './App.css';
+import { io } from "socket.io-client";
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Card from "react-bootstrap/Card";
+import CloseButton from "react-bootstrap/CloseButton";
+import Container from "react-bootstrap/Container";
+import Modal from "react-bootstrap/Modal";
+import Stack from "react-bootstrap/Stack";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [rooms, setRooms] = useState([]);
+  const [currentRoom, setCurrentRoom] = useState("");
+  const [show, setShow] = useState(true);
+
+  
+  const data = {
+    room: currentRoom,
+    username: username,
+    message: newMessage,
+    time: Date(),
+  };
+
+  useEffect(() => {
+    socket.emit("ready");
+
+    socket.on("receive_message", (data) => {
+      setMessages((messageList) => [...messageList, data]);
+
+    });
+    socket.on("get_rooms", (data) => {
+      setRooms(data);
+    });
+  }, []);
+
+  function sendMessage() {
+    if (data.message === "") {
+      alert("No empty messages, thank you!");
+    } else {
+      socket.emit("send_message", data);
+      setMessages((messages) => [...messages, data]);
+    }
+  }
+
+  const joinRoom = (room) => {
+    socket.emit("join_room", room);
+    if (username !== "" && room !== "") {
+    }
+    setCurrentRoom(room);
+  };
+
+  const saveUsername = (username) => {
+    socket.emit("save_username", username);
+    if (username !== "") {
+      setShow(false);
+    }
+  };
+
+  const deleteRoom = (room) => {
+    socket.emit("delete_room", room);
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+const leaveRoom = (room) => {
+  socket.emit("leave_room", room);
+  setRoom("");
 }
 
+  return (
+
+      <Container>
+        <Modal show={show}>
+          <Modal.Body>
+            <Form.Control
+              onSubmit={handleSubmit}
+              placeholder="Username.."
+              type="text"
+              onChange={(e) => setUsername(e.target.value)}
+            />{" "}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button type="submit" onClick={() => saveUsername(username)}>
+              Save Username
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <Row>
+          <Col>
+        <Stack direction="horizontal" gap={6}>
+        <Form.Control
+          onSubmit={handleSubmit}
+          placeholder="Create new chat room.."
+          type="text"
+          onChange={(e) => {
+            setRoom(e.target.value);
+          }}
+        />
+        <Button type="submit" onClick={() => joinRoom(room)}>
+          Save
+        </Button>
+        </Stack>
+        </Col>
+        <Col><h5>Hi {username}! You have joined chat: {currentRoom}.</h5>
+        <Button type="reset" onClick={() => leaveRoom(room)}>Leave Room</Button></Col>
+        </Row>
+        <Row>
+          <Col>
+          <Stack style={{
+      maxHeight: "400px", minHeight: "400px",
+      overflowY: "auto", border: "gray solid 0.5px", borderRadius: "4px"}}>
+        <h5>JOIN ROOM</h5>
+        <Stack>
+          {rooms.map((room) => (
+            <Button variant="light"
+              className={`roomDivTwo ${
+                currentRoom === room.room ? "current" : null
+              }`}
+              key={room.id}
+            >
+              <Stack direction="horizontal" gap={6}>
+                <Card onClick={() => joinRoom(room.room)}>{room.room}</Card>
+                <CloseButton onClick={() => deleteRoom(room.room)} />
+              </Stack>
+            </Button>
+          ))}
+        </Stack>
+        </Stack>
+        </Col>
+        <Col>
+        <Stack style={{
+      maxHeight: "400px", minHeight: "400px",
+      overflowY: "auto", border: "gray solid 0.5px", borderRadius: "4px"}}>
+          {messages.map((message) => {
+            return (
+              <Card key={message.id}>
+                <Card.Body>
+                  <Card.Title>{message.username}</Card.Title>
+                  <Card.Text>{message.message}</Card.Text>
+                  <Card.Subtitle>{message.time}</Card.Subtitle>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </Stack>
+        </Col>
+        </Row>
+        <Row>
+        <FloatingLabel controlId="" label="Hey..">
+          <Form.Control
+            as="textarea"
+            onSubmit={handleSubmit}
+            placeholder="Hey.."
+            onChange={(e) => {
+              setNewMessage(e.target.value);
+            }}
+          />
+        </FloatingLabel>
+        <Button type="submit" className="sendButton" onClick={sendMessage}>
+          Send
+        </Button>
+        </Row>
+      </Container>
+   
+  );
+}
 export default App;
