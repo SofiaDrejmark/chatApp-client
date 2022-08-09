34fe426ef8cf2225de056fa0188aa31cc8e426dc
SofiaDrejmark <sofiadrejmark@gmail.com>
2022-08-09 12:46:41 +0200

    messages to right room

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import CloseButton from "react-bootstrap/CloseButton";
 import Container from "react-bootstrap/Container";
 import Modal from "react-bootstrap/Modal";
 import Stack from "react-bootstrap/Stack";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const socket = io.connect("http://localhost:3001");
 
@@ -23,7 +23,6 @@ function App() {
   const [currentRoom, setCurrentRoom] = useState("");
   const [show, setShow] = useState(true);
 
-  
   const data = {
     room: currentRoom,
     username: username,
@@ -36,10 +35,12 @@ function App() {
 
     socket.on("receive_message", (data) => {
       setMessages((messageList) => [...messageList, data]);
-
     });
     socket.on("get_rooms", (data) => {
       setRooms(data);
+    });
+    socket.on("get_messeges", (data) => {
+      setMessages(data);
     });
   }, []);
 
@@ -48,11 +49,12 @@ function App() {
       alert("No empty messages, thank you!");
     } else {
       socket.emit("send_message", data);
-      setMessages((messages) => [...messages, data]);
+      
     }
   }
 
   const joinRoom = (room) => {
+    console.log("join room");
     socket.emit("join_room", room);
     if (username !== "" && room !== "") {
     }
@@ -74,90 +76,106 @@ function App() {
     e.preventDefault();
   }
 
-const leaveRoom = (room) => {
-  socket.emit("leave_room", room);
-  setRoom("");
-}
+  const leaveRoom = (room) => {
+    socket.emit("leave_room", room);
+    setRoom("");
+  };
 
   return (
-
-      <Container>
-        <Modal show={show}>
-          <Modal.Body>
+    <Container>
+      <Modal show={show}>
+        <Modal.Body>
+          <Form.Control
+            onSubmit={handleSubmit}
+            placeholder="Username.."
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+          />{" "}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button type="submit" onClick={() => saveUsername(username)}>
+            Save Username
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Row>
+        <Col>
+          <Stack direction="horizontal" gap={6}>
             <Form.Control
               onSubmit={handleSubmit}
-              placeholder="Username.."
+              placeholder="Create new chat room.."
               type="text"
-              onChange={(e) => setUsername(e.target.value)}
-            />{" "}
-          </Modal.Body>
-          <Modal.Footer>
-            <Button type="submit" onClick={() => saveUsername(username)}>
-              Save Username
+              onChange={(e) => {
+                setRoom(e.target.value);
+              }}
+            />
+            <Button type="submit" onClick={() => joinRoom(room)}>
+              Save
             </Button>
-          </Modal.Footer>
-        </Modal>
-        <Row>
-          <Col>
-        <Stack direction="horizontal" gap={6}>
-        <Form.Control
-          onSubmit={handleSubmit}
-          placeholder="Create new chat room.."
-          type="text"
-          onChange={(e) => {
-            setRoom(e.target.value);
-          }}
-        />
-        <Button type="submit" onClick={() => joinRoom(room)}>
-          Save
-        </Button>
-        </Stack>
-        </Col>
-        <Col><h5>Hi {username}! You have joined chat: {currentRoom}.</h5>
-        <Button type="reset" onClick={() => leaveRoom(room)}>Leave Room</Button></Col>
-        </Row>
-        <Row>
-          <Col>
-          <Stack style={{
-      maxHeight: "400px", minHeight: "400px",
-      overflowY: "auto", border: "gray solid 0.5px", borderRadius: "4px"}}>
-        <h5>JOIN ROOM</h5>
-        <Stack>
-          {rooms.map((room) => (
-            <Button variant="light"
-              className={`roomDivTwo ${
-                currentRoom === room.room ? "current" : null
-              }`}
-              key={room.id}
-            >
-              <Stack direction="horizontal" gap={6}>
-                <Card onClick={() => joinRoom(room.room)}>{room.room}</Card>
-                <CloseButton onClick={() => deleteRoom(room.room)} />
-              </Stack>
-            </Button>
-          ))}
-        </Stack>
-        </Stack>
+          </Stack>
         </Col>
         <Col>
-        <Stack style={{
-      maxHeight: "400px", minHeight: "400px",
-      overflowY: "auto", border: "gray solid 0.5px", borderRadius: "4px"}}>
-          {messages.map((message) => {
-            return (
-              <Card key={message.id}>
-                <Card.Body>
-                  <Card.Title>{message.username}</Card.Title>
-                  <Card.Text>{message.message}</Card.Text>
-                  <Card.Subtitle>{message.time}</Card.Subtitle>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </Stack>
+          <h5>
+            Hi {username}! You have joined chat: {currentRoom}.
+          </h5>
+
         </Col>
-        </Row>
-        <Row>
+      </Row>
+      <Row>
+        <Col>
+          <Stack
+            style={{
+              maxHeight: "400px",
+              minHeight: "400px",
+              overflowY: "auto",
+              border: "gray solid 0.5px",
+              borderRadius: "4px",
+            }}
+          >
+            <h5>JOIN ROOM</h5>
+            <Stack>
+              {rooms.map((room) => (
+                <Button
+                  variant="light"
+                  className={`roomDivTwo ${
+                    currentRoom === room.room ? "current" : null
+                  }`}
+                  key={room.id}
+                >
+                  <Stack direction="horizontal" gap={6}>
+                    <Card onClick={() => joinRoom(room.room)}>{room.room}</Card>
+                    <CloseButton onClick={() => deleteRoom(room.room)} />
+                  </Stack>
+                </Button>
+              ))}
+            </Stack>
+          </Stack>
+        </Col>
+        <Col>
+          <Stack
+            style={{
+              maxHeight: "400px",
+              minHeight: "400px",
+              overflowY: "auto",
+              border: "gray solid 0.5px",
+              borderRadius: "4px",
+            }}
+          >
+            {messages.map((message) => {
+              return (
+                <Card key={message.id}>
+                  <Card.Body>
+                    <Card.Title>{message.username}</Card.Title>
+                    <Card.Text>{message.message}</Card.Text>
+                    <Card.Subtitle>{message.time}</Card.Subtitle>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </Stack>
+        </Col>
+      </Row>
+      <Row>
         <FloatingLabel controlId="" label="Hey..">
           <Form.Control
             as="textarea"
@@ -171,9 +189,8 @@ const leaveRoom = (room) => {
         <Button type="submit" className="sendButton" onClick={sendMessage}>
           Send
         </Button>
-        </Row>
-      </Container>
-   
+      </Row>
+    </Container>
   );
 }
 export default App;
